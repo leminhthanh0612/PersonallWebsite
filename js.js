7e0d36d4cd39dee6cc2c5e28923a62808f13f3ae
ThanhLeMinh <leminhthanh0612@gmail.com>
2019-05-23 22:29:18 +0700

    Update autoplay video button

--- js.js
+++ js.js
@@ -1,39 +1,28 @@
-﻿var images = ["images/pro1.jpg","images/pro2.jpg","images/pro3.jpg"];
-var index = 0;
-function show()
-{
-	var img = document.getElementById("img");
-	img.src = images[index++];
-	
-	if(index >= images.length)
-		index = 0;
-		
-	setTimeout("show()",5000);
-}
+﻿
 
 function showtime()
 { 
-  if (!document.all&&!document.getElementById) 
-    return 
-  thelement=document.getElementById ? document.getElementById("tick2") : document.all.tick2;
-  var Digital=new Date(); 
-  var hours=Digital.getHours();
-  var minutes=Digital.getMinutes();
-  var seconds=Digital.getSeconds(); 
-  var dn="PM"; 
-  if (hours<12) 
-    dn="AM" 
-  if (hours>12) 
-    hours=hours-12; 
-  if (hours==0) 
-    hours=12; 
-  if (minutes<=9) 
-    minutes="0"+minutes; 
-  if (seconds<=9) 
-    seconds="0"+seconds; 
-  var ctime=hours + ":" + minutes + ":" + seconds + " " +dn 
-  thelement.innerHTML="<b style='font-size:30;color:#A8CCDC;'>"+ctime+"</b>";
-  setTimeout("showtime()",1000);
+  //if (!document.all&&!document.getElementById) 
+  //  return 
+  //thelement=document.getElementById ? document.getElementById("tick2") : document.all.tick2;
+  //var Digital=new Date(); 
+  //var hours=Digital.getHours();
+  //var minutes=Digital.getMinutes();
+  //var seconds=Digital.getSeconds(); 
+  //var dn="PM"; 
+  //if (hours<12) 
+  //  dn="AM" 
+  //if (hours>12) 
+  //  hours=hours-12; 
+  //if (hours==0) 
+  //  hours=12; 
+  //if (minutes<=9) 
+  //  minutes="0"+minutes; 
+  //if (seconds<=9) 
+  //  seconds="0"+seconds; 
+  //var ctime=hours + ":" + minutes + ":" + seconds + " " +dn 
+  //thelement.innerHTML="<b style='font-size:30;color:#A8CCDC;'>"+ctime+"</b>";
+  //setTimeout("showtime()",1000);
 }
 
 function showday()
@@ -93,11 +82,16 @@ $(function () {
     var pause = $('.pause');
     var play = $('.play');
 
+    var mute = $('.mute');
+    var unmute = $('.unmute');
+
     if (window.matchMedia('(prefers-reduced-motion)').matches) {
         vid.removeAttribute("autoplay");
         vid.pause();
        // pauseButton.innerHTML = "Paused";
         play.hide();
+
+        
     }
 
     function vidFade() {
@@ -135,6 +129,25 @@ $(function () {
             pause.hide();
         }
     });
+
+
+    mute.hide();
+    $('.mute-unmute').on('click', function () {
+        vid.classList.toggle("stopfade");
+        if (vid.muted) {
+            vid.muted = false;
+            mute.show();
+            unmute.hide();
+        } else {
+            vid.muted = true
+            unmute.show();
+            mute.hide();
+        }
+    });
+
+
+
+
 
 
     // Select all links with hashes
